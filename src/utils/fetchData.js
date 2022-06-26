0fa76edf7bb9aebab80c6e2497aa3fead53d93bb
mohamedtarek427 <102771793+mohamedtarek427@users.noreply.github.com>
2022-06-26 01:23:03 -0700

    create commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions =  {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+      'X-RapidAPI-Key': 'bcaf5869bdmsh1dbd816f42a9f7fp166d6ejsnfb2eebcefa72',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
